0e4289cc2c7bf3557be21a5f83a3aa4f187da87f
Algis Dumbris <a.dumbris@gmail.com>
2018-04-12 18:32:41 +0300

    first commit

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## Docs here https://github.com/asciidoctor/docker-asciidoctor
+
+docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -r asciidoctor-mathematical -r asciidoctor-diagram *.adoc
+docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-mathematical -r asciidoctor-diagram *.adoc
+#asciidoctor -r asciidoctor-diagram *.adoc
